1f35fb80fe6f053e566eb7ed5a02c60856c882e5
fruitnuke <dan@amygda.la>
2016-04-03 12:41:54 -0400

    Sample programs to run count-instructions against.

---
+++ samples/count/sample-01.c
@@ -0,0 +1,13 @@
+/* A simple test program to run count-instructions against.
+ *
+ * As this loads and executes the c runtime, there are actually quite
+ * a lot of instructions executed!
+ */
+
+#include <stdio.h>
+
+
+int main(int argc, char** argv)
+{
+    printf("Hello world.\n");
+}
