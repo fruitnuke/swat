1e3b8cc934adeb8927cac888823c9f9e05fb3b4e
fruitnuke <dan@amygda.la>
2016-04-03 12:42:56 -0400

    Run program named on command line as a child process.

--- samples/count/count-instructions.c
+++ samples/count/count-instructions.c
@@ -8,7 +8,25 @@
  * each time an instruction in the "debuggee" is executed.
  */
 
+#include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <sys/user.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+
+/* Print messages to stdout, prefixed with the processes pid. */
+void procmsg(const char* format, ...)
+{
+    printf("[%u] ", getpid());
+    va_list ap;
+    va_start(ap, format);
+    vprintf(format, ap);
+    va_end(ap);
+}
 
 
 int main(int argc, char** argv)
@@ -16,5 +34,29 @@ int main(int argc, char** argv)
     if (argc < 2) {
         fprintf(stderr, "Expected a program name as argument.\n");
         return -1;
+    }
+
+    const char* progname = argv[1];
+    pid_t pid;
+
+    if ((pid = fork()) < 0) {
+        perror("fork");
+        return -1;
+    }
+
+    if (pid == 0) {
+        // Child process, the debuggee.
+        procmsg("starting %s.\n", progname);
+
+        execl(progname, progname, 0);
+    }
+    else {
+        // Parent process, the debugger.
+        procmsg("starting debugger.\n");
+
+        int status;
+        waitpid(pid, &status, 0);
+
+        procmsg("debugger exiting.\n");
     }
 }
